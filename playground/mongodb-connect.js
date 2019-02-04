4d6f194ad643ead1f296a8493f8cc695ef43d391
ahmed <sobh6081@gmail.com>
2019-02-04 02:56:33 +0200

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// Loading modules
+const { MongoClient, ObjectID } = require('mongodb');
+
+// let id = new ObjectID();
+// console.log(id);
+
+// Make a connection
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (error, client) => {
+  if (error) return console.log('Unable to connect to mongoDB server');
+  console.log('Connected to mongoDB server');
+
+  const db = client.db('TodoApp');
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (error, result) => {
+  //   if (error) return console.log('Unable to insert todo', error);
+  //   console.log(result.ops);
+  // });
+  
+  
+  db.collection('Users').insertOne({
+    // _id: 'asdf',
+    name: 'Ahmed',
+    age: 22,
+    location: 'Egypt'
+  }, (error, result) => {
+    if (error) console.log('Unable to insert to mongoDB database.');
+    else console.log(result.ops[0]._id.getTimestamp().toLocaleString());
+  });
+
+  client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// Loading modules
+const { MongoClient, ObjectID } = require('mongodb');
+
+// let id = new ObjectID();
+// console.log(id);
+
+// Make a connection
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (error, client) => {
+  if (error) return console.log('Unable to connect to mongoDB server');
+  console.log('Connected to mongoDB server');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   // completed: false,
+  //   // _id: new ObjectID('5c56a2aa26a65d181825c22f'),
+  //   completed: true
+  // }).toArray().then(collection => {
+  //   console.log('Todos', collection);
+  // }, error => {
+  //   console.log('Unable to fetch collection', error);
+  // });
+
+
+  db.collection('Users').find({
+    name: 'Ahmed'
+  }).count((error, result) => {
+    if (error) console.log('Unable to query Users count', error);
+    else console.log(`Users count: ${result}`);
+  });
+
+  // client.close();
+});
