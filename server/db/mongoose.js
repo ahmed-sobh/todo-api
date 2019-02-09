92a0a915f2b734df946cffa496a71c125fd46eb2
ahmed <sobh6081@gmail.com>
2019-02-09 14:11:39 +0200

    Add PATCH /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,8 +7,11 @@ mongoose.Promise = global.Promise;
 // Making the connection
 // Require CC to push app up to heroku and get real database uri to get the job done
 var uri = process.env.MONGODB_URI || `mongodb://localhost:27017/TodoApp`;
-mongoose.connect(uri, {
-  useNewUrlParser: true
-});
+var options = {
+  useNewUrlParser: true,
+  useFindAndModify: false
+};
+
+mongoose.connect(uri, options);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
+var _ = require('lodash');
 
 // Loading custom modules
 var {mongoose} = require('./db/mongoose');
@@ -71,6 +72,38 @@ app.delete('/todos/:id', (req, res, next) => {
       console.log('NOTFOUND');
     }
   }, error => {
+    res.status(400).send();
+  });
+
+});
+
+
+app.patch('/todos/:id', (req, res, next) => {
+
+  var id = req.params.id;
+  // IMPORTAND: create an object from the picked object props IF EXIST!
+  var body = _.pick(req.body, ['text', 'completed']);
+  console.log(body);
+  if (!ObjectID.isValid(id)) return res.status(404).end();
+  // console.log(completed, typeof completed);
+
+  if (_.isBoolean(body.completed) && body.completed) {
+
+    body.completedAt = new Date().getTime();
+
+  } else {
+
+    body.completed = false;
+    body.completedAt = null;
+
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {new: true}).then(todo => {
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  }, err => {
     res.status(400).send();
   });
 
